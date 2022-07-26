95d6e7e64e3bcccd2f6ae35b8f4008f938943bed
iostate <qmtruong92@gmail.com>
2022-07-25 22:47:58 -0700

    git add

--- twosum.js
+++ twosum.js
@@ -1,19 +1,15 @@
 const findTwoSum = function (nums, target) {
-  let indexes = [];
   for (let i = 0; i < nums.length; i++) {
     const numToFind = target - nums[i];
     for (let ii = i + 1; ii < nums.length; ii++) {
-      console.log(`${nums[i]} + ${nums[ii]}`);
-      console.log(nums[i] + nums[ii]);
-      console.log(numToFind + '\n');
       if (numToFind == nums[ii]) {
-        return indexes;
+        return [i, ii];
       }
     }
   }
   return null;
 };
-let test1 = [1, 3, 7, 14, 2];
+let test1 = [1, 3, 7, 9, 2];
 let test2 = [1, 3, 14, 11, 14];
 const result = findTwoSum(test1, 11);
 const result2 = findTwoSum(test2, 13);
